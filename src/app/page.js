ae5e0ce9587e1581c4bdbb1dbe3620c3737c44e4
Nishmi Dilshani Dissanayaka <nishudissanayaka15@gmail.com>
2026-08-27 21:47:25 +0530

    Initial commit - Student Management System Client

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
