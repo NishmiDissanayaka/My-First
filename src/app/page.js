e8ffc311fe3bbdf89e4942fd87441b1e36572988
Nishmi Dilshani Dissanayaka <nishudissanayaka15@gmail.com>
2026-09-01 09:23:05 +0530

    Add Hello World page

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,7 @@
-import React from 'react'
-
 export default function Home() {
   return (
-    <div style={{
-      backgroundColor: '#ffffff',
-      color: '#000000',
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <h1 style={{ fontSize: '48px', fontWeight: 'bold', margin: '0' }}>
-        HELLO WORLD
-      </h1>
-    </div>
-  )
+    <main>
+      <h1>Hello World</h1>
+    </main>
+  );
 }
